d31df63e42f75b39b8a1a075782f6d3c4ae0d4dc
weiyihai <dhwl1234@163.com>
2023-03-16 22:05:49 +0800

    1

--- Question_One/main.py
+++ Question_One/main.py
@@ -1,1 +1,10 @@
 import numpy as np
+import time
+import math
+
+
+x = [i * 0.001 for i in range(1000000)]
+start = time.clock()
+for i, t in enumerate(x):
+    x[i] = math.sin(t)
+print("math.sin:", time.clock() - start)
