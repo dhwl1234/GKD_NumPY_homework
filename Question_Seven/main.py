5778033e0f0e984a482d850fb159373566d6b5b2
weiyihai <dhwl1234@163.com>
2023-03-22 00:11:36 +0800

    2023.3.22

--- Question_Seven/main.py
+++ Question_Seven/main.py
@@ -1,30 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from numpy.polynomial import Chebyshev
 
-# 定义目标函数g(x)
 def g(x):
     z = (x - 1) * 5
     return np.sin(z**2) + np.sin(z)**2
 
-# 指定插值区间和节点数
-a, b = -1, 1
-n = 100
 
-# 在切比雪夫节点上插值
-cheb_nodes = np.polynomial.chebyshev.chebpts1(n, (a, b))
-cheb_fit = np.polynomial.chebyshev.Chebyshev.fit(cheb_nodes, g(cheb_nodes), n)
-cheb_error = np.abs(g(np.linspace(a, b, 1000)) - cheb_fit(np.linspace(a, b, 1000))).max()
+n = 101
+# Generate node
+x = np.linspace(-1,1,n)
+cheb_nodes = Chebyshev.basis(n).roots()
+xd = np.linspace(-1,1,1000)
 
-# 在等距节点上插值
-poly_nodes = np.linspace(a, b, n)
-poly_fit = np.polynomial.Polynomial.fit(poly_nodes, g(poly_nodes), n-1)
-poly_error = np.abs(g(np.linspace(a, b, 1000)) - poly_fit(np.linspace(a, b, 1000))).max()
+# Interpolation calculate
+cheb_fit = Chebyshev.fit(cheb_nodes,g(cheb_nodes),n-1,domain=[-1,1])
+poly_fit = np.polynomial.Polynomial.fit(x,g(x),n-1,domain=[-1,1])
 
-# 绘制插值结果和误差图像
-x = np.linspace(a, b, 1000)
-plt.plot(x, g(x), label='g(x)')
-plt.plot(x, cheb_fit(x), label='Chebyshev fit')
-plt.plot(x, poly_fit(x), label='Polynomial fit')
+# Calculate error
+cheb_error = np.abs(g(xd) - cheb_fit(xd)).max()
+poly_error = np.abs(g(xd)-poly_fit(xd)).max()
+
+print(u"插值多项式的最大误差："),
+print(u"ploynomial点：", poly_error),
+print(u"chebyshev：",cheb_error)
+# Draw picture
+plt.plot(xd, g(xd), label='g(x)')
+plt.plot(xd, cheb_fit(xd), label='Chebyshev fit')
+plt.plot(xd, poly_fit(xd), label='Polynomial fit')
 plt.legend()
 
 plt.figure()
