77fee32de0555834efc12afc8157e47b0952bd12
weiyihai <dhwl1234@163.com>
2023-03-22 10:25:32 +0800

    2023.3.22

--- Question_Seven/main.py
+++ Question_Seven/main.py
@@ -11,15 +11,16 @@ n = 101
 # Generate node
 x = np.linspace(-1,1,n)
 cheb_nodes = Chebyshev.basis(n).roots()
+poly_nodes = np.polynomial.Polynomial.basis(n).roots()
 xd = np.linspace(-1,1,1000)
 
 # Interpolation calculate
 cheb_fit = Chebyshev.fit(cheb_nodes,g(cheb_nodes),n-1,domain=[-1,1])
-poly_fit = np.polynomial.Polynomial.fit(x,g(x),n-1,domain=[-1,1])
-
+#poly_fit = np.polynomial.Polynomial.fit(x,g(x),n-1,domain=[-1,1])
+poly_fit1= np.polynomial.Polynomial.fit(poly_nodes,g(poly_nodes),n-1,domain=[-1,1])
 # Calculate error
 cheb_error = np.abs(g(xd) - cheb_fit(xd)).max()
-poly_error = np.abs(g(xd)-poly_fit(xd)).max()
+poly_error = np.abs(g(xd)-poly_fit1(xd)).max()
 
 print(u"插值多项式的最大误差："),
 print(u"ploynomial点：", poly_error),
@@ -27,7 +28,7 @@ print(u"chebyshev：",cheb_error)
 # Draw picture
 plt.plot(xd, g(xd), label='g(x)')
 plt.plot(xd, cheb_fit(xd), label='Chebyshev fit')
-plt.plot(xd, poly_fit(xd), label='Polynomial fit')
+plt.plot(xd, poly_fit1(xd), label='Polynomial fit')
 plt.legend()
 
 plt.figure()
